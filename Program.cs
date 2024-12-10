82a9ccb713f9d7efb794362c00eed75869b3569f
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-12-09 23:26:15 -0600

    [1.15a] Display products for selecte category

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ do
 {
   Console.WriteLine("1) Display categories");
   Console.WriteLine("2) Add category");
+  Console.WriteLine("3) Display Category and related products");
   Console.WriteLine("Enter to quit");
   string? choice = Console.ReadLine();
   Console.Clear();
@@ -82,6 +83,21 @@ Category category = new();
 
 
 
+  }
+  else if (choice == "3")
+  {
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryId);
+    Console.WriteLine("Select the category whose products you want to display:");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+    int id = int.Parse(Console.ReadLine()!);
+    Console.Clear();
+    logger.Info($"CategoryId {id} selected");
   }
   else if (String.IsNullOrEmpty(choice))
   {
